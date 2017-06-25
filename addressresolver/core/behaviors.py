ba1b363d282dab004d90e50df886a234d4cc0c74
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-06-25 15:28:55 -0400

    perf: add singleton metaclass pattern

--- addressresolver/core/behaviors.py
+++ addressresolver/core/behaviors.py
@@ -3,6 +3,15 @@ import uuid
 from django.db import models
 
 
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 class Timestampable(models.Model):
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
