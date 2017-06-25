2aaa1300a8306c825bddb77a8d5d57665ce7b5a2
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-06-25 16:24:13 -0400

    test: add get elevation from lat and lng service

--- addressresolver/gmapservices/tests/test_services.py
+++ addressresolver/gmapservices/tests/test_services.py
@@ -16,6 +16,16 @@ def get_geocoding_object_with_lat_and_lng(lat, lng):
     }
 
 
+def get_elevation_with_lat_and_lng(elevation, lat, lng):
+    return {
+        "elevation": elevation,
+        "location": {
+            "lat": lat,
+            "lng": lng
+        }
+    }
+
+
 class GmapServicesTest(TestCase):
     def setUp(self):
         self.service = GmapServices()
@@ -80,3 +90,38 @@ class GmapServicesTest(TestCase):
 
         result = self.service.get_geocoding_from_address("address")
         self.assertEqual(result, None)
+
+    def test_elevation_from_lat_lng_return_if_find_one_or_more(self):
+        return_value = [get_elevation_with_lat_and_lng(1, 1, 2)]
+        client = MagicMock()
+        client.get_elevation.return_value = return_value
+        self.service.client = client
+
+        result = self.service.get_elevation_from_lat_and_lng(1, 2)
+        self.assertEqual(result, 1)
+
+    def test_elevation_from_lat_lng_none_if_empty(self):
+        return_value = []
+        client = MagicMock()
+        client.get_elevation.return_value = return_value
+        self.service.client = client
+
+        result = self.service.get_elevation_from_lat_and_lng(1, 2)
+        self.assertEqual(result, None)
+
+    def test_elevation_from_lat_lng_when_client_fails(self):
+        client = MagicMock()
+        client.get_elevation.return_value = None
+        self.service.client = client
+
+        result = self.service.get_elevation_from_lat_and_lng(1, 2)
+        self.assertEqual(result, None)
+
+    def test_elevation_from_lat_lng_get_first_always(self):
+        return_value = [get_elevation_with_lat_and_lng(1, 2, 3), get_elevation_with_lat_and_lng(4, 5, 6)]
+        client = MagicMock()
+        client.get_elevation.return_value = return_value
+        self.service.client = client
+
+        result = self.service.get_elevation_from_lat_and_lng(1, 2)
+        self.assertEqual(result, 1)
