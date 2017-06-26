206895ec7a0a5605b1ab3663e9db8a9c63aaa46a
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-06-26 01:42:23 -0400

    feat: add develop settings

--- addressresolver/config/settings/dev.py
+++ addressresolver/config/settings/dev.py
@@ -3,3 +3,14 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'addressresolver',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
