5bdd20fd1aa7e2d937125507fea538ffdb9ca0c0
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-06-25 20:52:12 -0400

    feat: add elevation latitude and longitude to the respond

--- addressresolver/address/serializers.py
+++ addressresolver/address/serializers.py
@@ -3,8 +3,12 @@ from rest_framework import serializers
 from .models import Address
 
 
-class AddressSerializer(serializers.ModelSerializer):
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    elevation = serializers.ReadOnlyField()
+    latitude = serializers.ReadOnlyField()
+    longitude = serializers.ReadOnlyField()
+    url = serializers.HyperlinkedIdentityField(view_name='api:address-detail', read_only=True)
 
     class Meta:
         model = Address
-        fields = ('id', 'address', )
+        fields = ('id', 'url', 'address', 'elevation', 'latitude', 'longitude')
