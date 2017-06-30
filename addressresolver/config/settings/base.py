5f706a0cca713ae85e8dfd5ad362130b2a9f908c
Freddy Rondon <freddy.rondon@mercadolibre.cl>
2017-06-30 10:44:32 -0400

    fix: remove secrets keys from settings

--- addressresolver/config/settings/base.py
+++ addressresolver/config/settings/base.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8$3^4rf1ohp(xkjn#r$h-xd2#d=fp==v=3@czr%g4b-&^zm5jf'
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
